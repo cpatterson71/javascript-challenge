c8a4c386f73f6a568fa59d736b93a11653e9602a
cpatterson71 <carl.pattersonsd@gmail.com>
2020-11-30 16:13:55 -0800

    updated html and app for final submission

--- static/app.js
+++ static/app.js
@@ -4,7 +4,7 @@ var tableBod = d3.select("#tbodyid")
 // from data.js
 var tableData = data;
 
-// Copy data into rows on HTML 
+// Copy data into rows on HTML upon loading
 function appendTable(data) {
     d3.select("tbody").html("");
     data.forEach((selection) => {
@@ -17,18 +17,37 @@ function appendTable(data) {
 }
 appendTable(tableData);
 
+//create the event for the filter button
 d3.select("#filter-btn").on("click", filterDate);
 
-// Set Filter button function to filter by date
+// Set Filter button function to filter by date, then for other items
 function filterDate() {
     d3.event.preventDefault();
     var date = d3.select("#datetime").property("value");
+    var city = d3.select("#city").property("value");
+    var state = d3.select("#state").property("value");
+    var country = d3.select("#country").property("value");
+    var shape = d3.select("#shape").property("value");
     var filterDateTime = tableData;
     if (date) {
         filterDateTime = filterDateTime.filter((row) => row.datetime === date);
     }
+    if (city){
+        filterDateTime = filterDateTime.filter((row) => row.city === city);
+    }
+    if (state){
+        filterDateTime = filterDateTime.filter((row) => row.state === state);
+    }
+    if (country){
+        filterDateTime = filterDateTime.filter((row) => row.country === country);
+    }
+    if (shape){
+        filterDateTime = filterDateTime.filter((row) => row.shape === shape);
+    }
 appendTable(filterDateTime);
 };
+
+//clear button to reload data
 var clearButton = d3.select("#clear-btn");
 clearButton.on("click", function(){
     location.reload();
